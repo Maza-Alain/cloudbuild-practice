dc6da1a72577b33082dfa8233412ae406af151b3
jorgealain.maza.contractor <jorgealain.maza.contractor@bbva.com>
2024-02-07 16:38:38 -0600

    tests8

--- sql-proxy/migrate.py
+++ sql-proxy/migrate.py
@@ -2,4 +2,5 @@ import os
 os.getenv('DATABASE_NAME')
 os.getenv('DATABASE_USER')
 os.getenv('DATABASE_PASS')
-os.getenv('INSTANCE_CONNECTION_NAME')
+os.getenv('DATABASE_PORT')
+# os.getenv('INSTANCE_CONNECTION_NAME')
