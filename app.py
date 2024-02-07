94f6f1a456c09b96b2da10e96a0dfff70cff06e4
jorgealain.maza.contractor <jorgealain.maza.contractor@bbva.com>
2024-02-07 17:00:35 -0600

    tests11

--- app.py
+++ app.py
@@ -7,11 +7,16 @@ import pymysql
 import os
 
 def obtener_conexion():
-    db_host = os.environ["DATABASE_HOST"]
+    db_host = '127.0.0.1'
     db_user = os.environ["DATABASE_USER"]
     db_pass = os.environ["DATABASE_PASS"]
     db_name = os.environ["DATABASE_NAME"]
     db_port = os.environ["DATABASE_PORT"]
+    # db_host = os.environ["DATABASE_HOST"]
+    # db_user = os.environ["DATABASE_USER"]
+    # db_pass = os.environ["DATABASE_PASS"]
+    # db_name = os.environ["DATABASE_NAME"]
+    # db_port = os.environ["DATABASE_PORT"]
     return pymysql.connect(host=db_host,
                                 user=db_user,
                                 password=db_pass,
